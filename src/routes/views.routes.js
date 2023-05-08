39d7416bc9140c0fc2dd50075f3d7d72e0c5c6df
Lucas Ariel Rotta <lucas.ariel.rotta@gmail.com>
2023-05-08 04:34:44 -0300

    Se agrega render estatico a home

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,13 +1,19 @@
 import { Router } from 'express';
 const router = Router();
+import ProductManager from "../managers/ProductManager.js";
+const productoManager = new ProductManager()
 
 
+router.get('/', async (req, res) => {
+    const data = await productoManager.getProducts();
+    console.log(data)
+    res.render('home', {data});
+});
 
-router.get('/', async (req,res)=>{
+router.get('/realtimeproducts', async (req,res)=>{
  
-    res.render('index')
+    res.render('realTimeProducts')
 })
-
 
 export {
     router
